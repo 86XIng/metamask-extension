a6682d09f9d9231d5a5031e8c5c5cc473196c879
Mark Stacey <markjstacey@gmail.com>
2021-02-04 23:56:38 -0330

    WIP

--- ui/app/contexts/i18n.js
+++ ui/app/contexts/i18n.js
@@ -6,32 +6,38 @@ import { getCurrentLocale } from '../ducks/metamask/metamask';
 import {
   getCurrentLocaleMessages,
   getEnLocaleMessages,
+  getLocaleMessages,
 } from '../ducks/locale/locale';
 
 export const I18nContext = createContext((key) => `[${key}]`);
 
-export const I18nProvider = (props) => {
+export const I18nProvider = ({ children, globalLocale }) => {
   const currentLocale = useSelector(getCurrentLocale);
+  const allLocales = useSelector(getLocaleMessages);
   const current = useSelector(getCurrentLocaleMessages);
   const en = useSelector(getEnLocaleMessages);
 
+  const localeKey = globalLocale || currentLocale;
+
   const t = useMemo(() => {
     return (key, ...args) =>
-      getMessage(currentLocale, current, key, ...args) ||
-      getMessage(currentLocale, en, key, ...args);
-  }, [currentLocale, current, en]);
+      (allLocales[localeKey] &&
+        getMessage(localeKey, allLocales[localeKey], key, ...args)) ||
+      getMessage(localeKey, current, key, ...args) ||
+      getMessage(localeKey, en, key, ...args);
+  }, [allLocales, localeKey, current, en]);
 
-  return (
-    <I18nContext.Provider value={t}>{props.children}</I18nContext.Provider>
-  );
+  return <I18nContext.Provider value={t}>{children}</I18nContext.Provider>;
 };
 
 I18nProvider.propTypes = {
   children: PropTypes.node,
+  globalLocale: PropTypes.string,
 };
 
 I18nProvider.defaultProps = {
   children: undefined,
+  globalLocale: undefined,
 };
 
 export class LegacyI18nProvider extends Component {
